f75478bc3c36140d14b82a4e1eb7d241e7ac6e54
René Reinertsen <rene@Renes-MacBook-Pro.local>
2020-03-03 12:47:31 +0100

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const forecast = require("./utils/forecast");
 const geocode = require("./utils/geocode");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
@@ -94,6 +96,6 @@ app.get("*", (req, res) => {
     errorMessage: "Page not found"
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up and running at port 3000");
+app.listen(port, () => {
+  console.log("Server is up and running at port " + port);
 });
